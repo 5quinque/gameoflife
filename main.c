235360252a03667afd3885353b0489ef397facf4
Ryan Linnit <ryan.linnit@outlook.com>
2018-03-22 15:55:50 +0000

    rows and columns now fill the screen

--- main.c
+++ main.c
@@ -6,9 +6,6 @@
 // srand(), rand()
 #include <stdlib.h>
 
-#define ROWS 30
-#define COLS 60
-
 void update();
 void printgame();
 void togglelife(int row, int col);
@@ -16,16 +13,16 @@ void addrandomlife(int s);
 void handleinput(int c);
 int countneighbour(int row, int col);
 
-int row_start, col_start;
-char board[ROWS][COLS];
-char tempboard[ROWS][COLS];
+int **board;
+int **tempboard;
+int boardsize;
 int pause = 0;
 int running = 1;
-int screen_rows, screen_cols;
+int screen_rows, screen_cols, game_columns;
 MEVENT event;
 
 int main() {
-  struct timespec ts = {0, 100000000L}; //ts.tv_sec = 0; ts.tv_nsec = 10000000L;
+  struct timespec ts = {0, 100000000L};
   int c;
 
   srand(time(NULL));
@@ -54,11 +51,31 @@ int main() {
   mousemask(ALL_MOUSE_EVENTS, NULL);
 
   getmaxyx(stdscr, screen_rows, screen_cols);
-  row_start = (screen_rows / 2 - ROWS / 2);
-  col_start = (screen_cols / 2 - COLS / 2);
+
+  game_columns = screen_cols / 2;
+
+  boardsize = screen_rows * sizeof(int*);
+  board = malloc(screen_rows * sizeof(int*));
+  tempboard = malloc(screen_rows * sizeof(int*));
+
+  for (int i = 0; i < screen_rows; i++) {
+    boardsize += game_columns * sizeof(int);
+    board[i] = malloc(game_columns * sizeof(int));
+    tempboard[i] = malloc(game_columns * sizeof(int));
+
+      for (int x = 0; x < game_columns; x++) {
+        board[i][x] = 0;
+      }
+      memcpy(tempboard[i], board[i], game_columns * sizeof(int));
+  }
+
+  mvprintw(0,0, "%zu", boardsize);
+  mvprintw(0,10, "%d", screen_rows);
+  mvprintw(0,20, "%d", screen_cols);
+  mvprintw(0,30, "%d", game_columns);
 
   // Plant the seed of life
-  addrandomlife(200);
+  addrandomlife(2000);
 
   while (running) {
     c = getch();
@@ -89,10 +106,10 @@ void handleinput(int c) {
     case ' ':
       // toggle pause
       pause ^= 1;
-      move (row_start - 1, 0);
+      move (0, 0);
       clrtoeol();
       if (pause)
-        mvprintw(row_start - 1, screen_cols / 2 - 3, "Paused");
+        mvprintw(0, screen_cols / 2 - 3, "Paused");
       break;
     case 'q':
       running = 0;
@@ -102,8 +119,8 @@ void handleinput(int c) {
 
 void update() {
   int count;
-  for (int y = 0; y < ROWS; y++) {
-    for (int x = 0; x < COLS; x++) {
+  for (int y = 0; y < screen_rows; y++) {
+    for (int x = 0; x < game_columns; x++) {
       count = countneighbour(y, x);
       if (board[y][x] == 1) {
         tempboard[y][x] = (count < 2 || count > 3) ? 0 : 1;
@@ -112,16 +129,25 @@ void update() {
       }
     }
   }
-  memcpy(board, tempboard, sizeof(tempboard));
+
+  for (int p = 0; p < screen_rows; p++) {
+    memcpy(board[p], tempboard[p], game_columns * sizeof(int));
+  }
 }
 
 void printgame() {
   int c;
-  for (int y = 0; y < ROWS; y++) {
-    for (int x = 0; x < COLS; x++) {
+  for (int y = 0; y < screen_rows; y++) {
+    for (int x = 0; x < game_columns; x++) {
       c = board[y][x] ^ 3;
       attron(COLOR_PAIR(c));
-      mvaddch(y + row_start, x + col_start, '.');
+      if (board[y][x]) {
+        mvaddch(y + 1, x * 2, '.');
+        mvaddch(y + 1, x * 2 + 1, '.');
+      } else {
+        mvaddch(y + 1, x * 2, '-');
+        mvaddch(y + 1, x * 2 + 1, '-');
+      }
       attroff(COLOR_PAIR(c));
     }
   }
@@ -131,7 +157,12 @@ int countneighbour(int row, int col) {
   int count = 0;
   for (int y = row - 1; y < row + 2; y++) {
     for (int x = col - 1; x < col + 2; x++) {
-      if (y == row && x == col) continue;
+      if (
+        (y | x) >> 31 ||
+        y > screen_rows -1 ||
+        x > game_columns - 1 ||
+        (y == row && x == col)
+      ) continue;
       if (board[y][x] == 1) count++;
     }
   }
@@ -139,8 +170,8 @@ int countneighbour(int row, int col) {
 }
 
 void togglelife(int row, int col) {
-  int brow = row - row_start;
-  int bcol = col - col_start;
+  int brow = row - 1;
+  int bcol = col / 2;
   if ( ~(brow | bcol) >> 31 ) 
     board[brow][bcol] ^= 1;
 }
@@ -148,8 +179,8 @@ void togglelife(int row, int col) {
 void addrandomlife(int sperm_count) {
   int r, c;
   for (int i = 0; i < sperm_count; i++) {
-    r = rand() % ROWS;
-    c = rand() % COLS;
+    r = rand() % screen_rows;
+    c = rand() % game_columns;
     board[r][c] = 1;
   }
 }
